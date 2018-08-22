da203cb94112ea9a54a27986c3d9f5f335c81bc8
vijayanrece <36220152+vijayanrece@users.noreply.github.com>
2018-08-22 01:16:15 -0700

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,9 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Vijayan's Space 2.0 is cool")
+option_b = os.getenv('OPTION_B', "Vijayan's Space 2.0 is not at all cool")
+option_c = os.getenv('OPTION_C', "Vijayan's Space 2.0 needs improvements")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
@@ -34,6 +35,7 @@ def hello():
         'index.html',
         option_a=option_a,
         option_b=option_b,
+        option_c=option_c,
         hostname=hostname,
         vote=vote,
     ))
