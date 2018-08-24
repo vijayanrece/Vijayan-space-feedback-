fd539597bc07df03c9ce04b143bd2c59b1b847c5
vijayanrece <36220152+vijayanrece@users.noreply.github.com>
2018-08-23 19:45:55 -0700

    Update app.js

--- result/views/app.js
+++ result/views/app.js
@@ -3,26 +3,31 @@ var socket = io.connect({transports:['polling']});
 
 var bg1 = document.getElementById('background-stats-1');
 var bg2 = document.getElementById('background-stats-2');
+var bg3 = document.getElementById('background-stats-3');
 
 app.controller('statsCtrl', function($scope){
   $scope.aPercent = 50;
   $scope.bPercent = 50;
+  $scope.cPercent = 50;
 
   var updateScores = function(){
     socket.on('scores', function (json) {
        data = JSON.parse(json);
        var a = parseInt(data.a || 0);
        var b = parseInt(data.b || 0);
+       var c = parseInt(data.c || 0);
 
-       var percentages = getPercentages(a, b);
+       var percentages = getPercentages(a, b, c);
 
        bg1.style.width = percentages.a + "%";
        bg2.style.width = percentages.b + "%";
+       bg3.style.width = percentages.c + "%";
 
        $scope.$apply(function () {
          $scope.aPercent = percentages.a;
          $scope.bPercent = percentages.b;
-         $scope.total = a + b;
+         $scope.cPercent = percentages.c;
+         $scope.total = a + b + c;
        });
     });
   };
@@ -36,14 +41,15 @@ app.controller('statsCtrl', function($scope){
   });
 });
 
-function getPercentages(a, b) {
+function getPercentages(a, b, c) {
   var result = {};
 
-  if (a + b > 0) {
-    result.a = Math.round(a / (a + b) * 100);
-    result.b = 100 - result.a;
+  if (a + b + c > 0) {
+    result.a = Math.round(a / (a + b + c) * 100);
+    result.b = Math.round(b / (a + b + c) * 100);
+    result.c = 100 - result.a - result.b;
   } else {
-    result.a = result.b = 50;
+    result.a = result.b = result.c = 50;
   }
 
   return result;
